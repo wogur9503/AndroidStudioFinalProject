da809d8125e3e00582f0c1c5571d3df13caee2f0
wogur9503 <wogur9503@naver.com>
2020-07-03 01:19:45 +0900

    안드로이드스튜디오 파이널 프로젝트1.0

---
+++ app/src/main/java/com/cookandroid/n2016316029final/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.n2016316029final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
